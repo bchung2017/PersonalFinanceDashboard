cbc3ada38ca1e4fa7dac9a40b8cf34296a132cc3
LAPTOP-N4UVVLMN\bchun <bchung2017@gmail.com>
2025-01-16 03:31:12 -0500

    Add project files.

---
+++ ExpenseTracker/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions.Include(x => x.Category).Where(y => y.Date >= StartDate && y.Date <= EndDate).ToListAsync();
+
+            int totalIncome = SelectedTransactions.Where(i => i.Category.Type == "Income").Sum(i => i.Amount);
+            ViewBag.TotalIncome = totalIncome.ToString("C0");
+
+            int totalExpense = SelectedTransactions.Where(i => i.Category.Type == "Expense").Sum(i => i.Amount);
+            ViewBag.TotalExpense = totalExpense.ToString("C0");
+
+            int balance =  totalIncome - totalExpense;
+            ViewBag.Balance = balance.ToString("C0");
+
+            return View();
+        }
+    }
+}
---
+++ ExpenseTracker/Program.cs
@@ -0,0 +1,34 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Dependency Injection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCfEx3RXxbf1x1ZFRGalhTTnVWUj0eQnxTdEBjWH5fcnZRRWRaWEJ3Vg==");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
